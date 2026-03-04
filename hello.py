c17dd076cf6963dce1a71d8bb1e79f5a0ca274f1
Matthew <150399297+boob41337@users.noreply.github.com>
2026-03-04 20:06:42 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,4 @@ def add(a,b): return a + b
 def add(a,b): return a - b
 def add(a,b): return a * b
 def add(a,b): return a : b
-def add(a,b): return a % b
-def add(a,b): return (a : b) * 100
-def add(a,b): return 2(a + b)
+
